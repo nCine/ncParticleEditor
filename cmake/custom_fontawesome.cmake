4dc6b5d258208fba7c5e6326c7f5005107d9c549
Angelo Theodorou <encelo@gmail.com>
2020-02-01 01:48:48 +0100

    Integrate latest changes from the template project
    - Parse config.h to detect nCine compilation config
    - Use the compilation configuration to only install required libraries
    - Avoid copying the nCine library if it is static
    - Fix macOS application rpath if the nCine library is static
    - Add an option to exclude MSVC system libraries from the installation
    - Fix Font Awesome download directory name

--- cmake/custom_fontawesome.cmake
+++ cmake/custom_fontawesome.cmake
@@ -1,10 +1,10 @@
 if(CUSTOM_WITH_FONTAWESOME)
 	# Download and unpack FontAwesome at configure time
-	configure_file(cmake/custom_fontawesome_download.in fontawsome-download/CMakeLists.txt)
+	configure_file(cmake/custom_fontawesome_download.in fontawesome-download/CMakeLists.txt)
 
 	execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
 		RESULT_VARIABLE result
-		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/fontawsome-download
+		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/fontawesome-download
 	)
 	if(result)
 		message(STATUS "CMake step for FontAwesome failed: ${result}")
@@ -13,7 +13,7 @@ if(CUSTOM_WITH_FONTAWESOME)
 
 	execute_process(COMMAND ${CMAKE_COMMAND} --build .
 		RESULT_VARIABLE result
-		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/fontawsome-download
+		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/fontawesome-download
 	)
 	if(result)
 		message(STATUS "Build step for FontAwesome failed: ${result}")
@@ -23,8 +23,11 @@ if(CUSTOM_WITH_FONTAWESOME)
 	if(FONTAWESOME_ERROR)
 		set(CUSTOM_WITH_FONTAWESOME FALSE)
 	else()
-		execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_BINARY_DIR}/fontawesome-src/webfonts/fa-brands-400.ttf ${PACKAGE_DATA_DIR}/data/fonts
-			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_BINARY_DIR}/fontawesome-src/webfonts/fa-regular-400.ttf ${PACKAGE_DATA_DIR}/data/fonts
-			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_BINARY_DIR}/fontawesome-src/webfonts/fa-solid-900.ttf ${PACKAGE_DATA_DIR}/data/fonts)
+		execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${PACKAGE_DATA_DIR}/data/fonts
+			COMMAND ${CMAKE_COMMAND} -E copy_if_different
+				${CMAKE_BINARY_DIR}/fontawesome-src/webfonts/fa-brands-400.ttf
+				${CMAKE_BINARY_DIR}/fontawesome-src/webfonts/fa-regular-400.ttf
+				${CMAKE_BINARY_DIR}/fontawesome-src/webfonts/fa-solid-900.ttf
+				${PACKAGE_DATA_DIR}/data/fonts)
 	endif()
 endif()
